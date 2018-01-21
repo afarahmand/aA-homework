d62e36e40a36575e6fadedddf8350f3c4e48f001
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-01-20 16:11:48 -0800

    Create Goal App Rails project (gapp)

--- W5D2/gapp/config/routes.rb
+++ W5D2/gapp/config/routes.rb
@@ -1,23 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/show'
-
-  get 'users/index'
-
-  get 'users/destroy'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
