e8f4927f4c81e503207c7ed7a8cfe148c013a1bd
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-01-21 15:32:00 -0800

    Complete user model specs

--- W5D2/gapp/spec/models/user_spec.rb
+++ W5D2/gapp/spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:password_digest)}
+    it {should validate_presence_of(:session_token)}
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_uniqueness_of(:session_token)}
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_length_of(:password).is_at_least(6)}
+  end
+
+  # Associations
+  describe 'associations' do
+    it { should have_many(:comments)}
+    it { should have_many(:goals)}
+  end
 end
